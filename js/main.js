55c0b1c8c5889dac7e335b0839cf5caa1031b328
esakkirajmari <esakkirajmari@gmail.com>
2022-12-20 22:32:05 +0530

    index.html

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function welcome(){
+var arr=[70,80,90,30,20,50,300];
+var min=arr[0];
+
+for(var i=0;i<arr.length;i++){
+    if (arr[i]<=min){
+        min=arr[i];
+    }
+}
+    console.log(min);
+}
+welcome();
